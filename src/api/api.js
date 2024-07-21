384ab04c2ff526eadcdd79f30fafe8cdc8b5cd72
Truong Le <truong.le@nuvoair.com>
2024-07-20 22:16:13 -0500

    update route

--- src/api/api.js
+++ src/api/api.js
@@ -2,9 +2,9 @@ import axios from "axios";
 import { API_URL } from "./config";
 
 export const register = (email, password) => {
-  return axios.post(`${API_URL}/register`, { email, password });
+  return axios.post(`${API_URL}/auth/register`, { email, password });
 };
 
 export const login = (email, password) => {
-  return axios.post(`${API_URL}/login`, { email, password });
+  return axios.post(`${API_URL}/auth/login`, { email, password });
 };
