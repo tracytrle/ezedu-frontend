2348fef290e7b7cd52292e89543f7ff296e28159
Truc Tran <tracytrle@gmail.com>
2024-08-12 23:27:00 -0500

    done method to fetch healthRecord for userprofile

--- src/components/profile/AdditionalHealthRecord.js
+++ src/components/profile/AdditionalHealthRecord.js
@@ -1,15 +1,33 @@
 import * as React from "react";
-
 import SelectedListItem from "../ui/SelectedListItem";
+import { useEffect, useState } from "react";
+import { getUserHealthRecord } from "../../api/api";
 
-const additionalHealthHistories = [
-  { label: "allergies", value: "N/A" },
-  { label: "medications", value: "N/A" },
-];
 const additionHealth = "additionalHealthRecord";
 
 export default function AdditionalHealthRecord() {
-  return (
-    <SelectedListItem list={additionalHealthHistories} title={additionHealth} />
-  );
+  const userId = localStorage.getItem("userId");
+  const [additionalRecord, setAdditionalRecor] = useState([]);
+
+  useEffect(() => {
+    getUserHealthRecord(userId).then((response) => {
+      const data = response.data;
+      console.log("data:", data);
+
+      const keyValuePairsMap = [
+        {
+          label: "allergies",
+          value: data.allergies ? data.allergies : "N/A",
+        },
+        {
+          label: "medications",
+          value: data.medications ? data.medications : "N/A",
+        },
+      ];
+
+      setAdditionalRecor(keyValuePairsMap);
+    });
+  }, [userId]); // The dependency array ensures this runs when userId changes
+
+  return <SelectedListItem list={additionalRecord} title={additionHealth} />;
 }
